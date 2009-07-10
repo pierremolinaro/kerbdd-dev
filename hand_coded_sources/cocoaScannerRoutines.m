a67520276bab4a4e6eb2a1c0e6ca26059db683fc
pm <pm@f8ab71b4-1aee-0310-9d02-8b9973a9c48c>
2009-07-10 14:18:50 +0000

    MAJ suite MAJ GALGAS

---
+++ hand_coded_sources/cocoaScannerRoutines.m
@@ -0,0 +1,41 @@
+//----------------------------------------------------------------------------*
+
+#import "lexiqueBDD-cocoa.h"
+
+//---------------------------------------------------------------------*
+
+void scanner_cocoa_routine_formerCompEgal (BOOL * ioScanningOk, UInt32 * ioValue) {
+  *ioValue = 0 ;
+}
+
+//---------------------------------------------------------------------*
+
+void scanner_cocoa_routine_formerCompNonEgal (BOOL * ioScanningOk, UInt32 * ioValue) {
+  *ioValue = 1 ;
+}
+
+//---------------------------------------------------------------------*
+
+void scanner_cocoa_routine_formerCompInfEgal (BOOL * ioScanningOk, UInt32 * ioValue) {
+  *ioValue = 2 ;
+}
+
+//---------------------------------------------------------------------*
+
+void scanner_cocoa_routine_formerCompInf (BOOL * ioScanningOk, UInt32 * ioValue) {
+  *ioValue = 3 ;
+}
+
+//---------------------------------------------------------------------*
+
+void scanner_cocoa_routine_formerCompSupEgal (BOOL * ioScanningOk, UInt32 * ioValue) {
+  *ioValue = 4 ;
+}
+
+//---------------------------------------------------------------------*
+
+void scanner_cocoa_routine_formerCompSup (BOOL * ioScanningOk, UInt32 * ioValue) {
+  *ioValue = 5 ;
+}
+
+//----------------------------------------------------------------------------*
